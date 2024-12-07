79ad30a1f99c23d99301f54a56cd8f7885d1bf08
PastelNinjaK <bernalraven11@gmail.com>
2024-12-06 20:28:23 -0700

    Add files via upload

---
+++ teamcode/Auto/Working/Turn.java
@@ -0,0 +1,120 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.robot.Robot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+
+@Autonomous(name = "turn", group = "Robot")
+public class Turn extends LinearOpMode {
+    private DcMotor leftDrive;
+    private DcMotor rightDrive;
+    private DcMotor armMotor;
+    private CRServo wheelServo;
+    private Servo wrist;
+
+
+    // Constants for arm and wrist
+    final double ARM_TICKS_PER_DEGREE = 19.7924893140647; //exact fraction is (194481/9826)
+    double timePerTile = 0.900140347916205 * 0.2;
+
+
+
+
+    final double ARM_COLLAPSED_INTO_ROBOT  = 0;
+    final double ARM_COLLECT               = 250 * ARM_TICKS_PER_DEGREE;
+    final double ARM_CLEAR_BARRIER         = 230 * ARM_TICKS_PER_DEGREE;
+    final double ARM_SCORE_SPECIMEN        = 160 * ARM_TICKS_PER_DEGREE;
+    final double ARM_SCORE_SAMPLE_IN_LOW   = 160 * ARM_TICKS_PER_DEGREE;
+    final double ARM_ATTACH_HANGING_HOOK   = 120 * ARM_TICKS_PER_DEGREE;
+    final double ARM_WINCH_ROBOT           = 15  * ARM_TICKS_PER_DEGREE;
+
+
+    /* Variables to store the speed the intake servo should be set at to intake, and deposit game elements. */
+    final double INTAKE_COLLECT    = -1.0;
+    final double INTAKE_OFF        =  0.0;
+    final double INTAKE_DEPOSIT    =  0.5;
+
+
+    /* Variables to store the positions that the wrist should be set to when folding in, or folding out. */
+    final double WRIST_FOLDED_IN   = 0.8333;
+    final double WRIST_FOLDED_OUT  = 0.5;
+
+
+    @Override
+    public void runOpMode() {
+        // Initialize hardware
+        leftDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        armMotor = hardwareMap.get(DcMotor.class, "left_arm");
+        wrist = hardwareMap.get(Servo.class, "wrist");
+        wheelServo = hardwareMap.get(CRServo.class, "intake");
+
+
+        // Configure hardware
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        ((DcMotorEx) armMotor).setCurrentAlert(5, CurrentUnit.AMPS);
+        armMotor.setTargetPosition(0);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        wrist.setPosition(WRIST_FOLDED_IN);
+        wheelServo.setPower(INTAKE_OFF);
+
+
+        waitForStart();
+
+
+        if (opModeIsActive()) {
+            // Fill this up with the program
+            program(1.0,-1.0,2,ARM_SCORE_SPECIMEN,WRIST_FOLDED_IN,INTAKE_OFF);//Robot turns on
+
+            // Example telemetry feedback
+            if (((DcMotorEx) armMotor).isOverCurrent()) {
+                telemetry.addLine("MOTOR EXCEEDED CURRENT LIMIT!");
+            }
+            telemetry.addData("Arm Target: ", armMotor.getTargetPosition());
+            telemetry.addData("Arm Encoder: ", armMotor.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+
+
+    // Method to control all motors and servos
+    public void program(double leftPower, double rightPower, double delay, double armPosition, double wristPosition, double intake) {
+        leftDrive.setPower(leftPower);
+        rightDrive.setPower(rightPower);
+
+
+        if (armMotor.getTargetPosition() != (int) armPosition) {
+            armMotor.setTargetPosition((int) armPosition);
+            ((DcMotorEx) armMotor).setVelocity(2100);
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+
+
+        wrist.setPosition(wristPosition);
+        wheelServo.setPower(intake);
+
+
+        sleep((long) (delay * 1000));
+
+
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+    }
+
+
+    // Method to calculate time delay
+    public double TimeDelay(double dist, double speed) {
+        return dist / speed; // Distance in meters, speed in meters/second
+    }
+}
