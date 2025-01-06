7c8ac1efd615363687c2ee23a75d8ef815e112e7
PastelNinjaK <bernalraven11@gmail.com>
2025-01-06 16:39:20 -0700

    Update AutoOP2425AT.java

--- teamcode/Auto/Working/AutoOP2425AT.java
+++ teamcode/Auto/Working/AutoOP2425AT.java
@@ -1,1 +1,54 @@
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.robot.Robot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+@Autonomous(name = "AutoOP2425AT", group = "Robot")
+public class TeleOp2425M1 extends LinearOpMode {
+  private DcMotor LFDrive;
+  private DcMotor RFDrive;
+  private DcMotor LRDrive;
+  private DcMotor RRDrive;
+  private DcMotor SlideMotor;
+  private Servo intake; // Declare the servo
+  private Servo wrist;
+
+
+    @Override
+  public void runOpMode() {
+    // Initialize motors
+    LFDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
+    RFDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+    LRDrive = hardwareMap.get(DcMotor.class, "left_rear_drive");
+    RRDrive = hardwareMap.get(DcMotor.class, "right_rear_drive");
+    SlideMotor = hardwareMap.get(DcMotor.class, "slide_motor");
+    wrist = hardwareMap.get(Servo.class, "wrist_servo");
+    intake = hardwareMap.get(Servo.class, "intake_servo");
+    wrist.setPosition(0);// to be adjusted
+    //Brake mode
+    LFDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    LRDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    RFDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    RRDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    SlideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    // Reverse necessary motor direction
+
+
+;
+    waitForStart();
+
+    if (opModeIsActive()) {
+            
+    
+      }
+    }
+  }
+
+}
