39b25e3a9ea561a101fc5c1a33df70aa25830094
PastelNinjaK <bernalraven11@gmail.com>
2025-01-06 16:48:51 -0700

    Update TeleOp2425M1.java

--- teamcode/TeleOP/TeleOp2425M1.java
+++ teamcode/TeleOP/TeleOp2425M1.java
@@ -46,51 +46,44 @@ public class TeleOp2425M1 extends LinearOpMode {
       while (opModeIsActive()) {
         // Drive control
         double y = gamepad1.left_stick_y;
-        double x = gamepad1.left_stick_x;
+        double x1 = gamepad1.left_stick_x;
+        double x2 = gamepad.right_stick_x;
         double LFPower;
         double RFPower;
         double LRPower;
         double RRPower;
-        double slidePower;
-        int drive = 0;
+        double slidePower = gamepad1.right_trigger - gamepad1.left_trigger;
+        int wristServo = 0;
         if(gamepad1.a){
-          drive++;
+          wristServo++;
         }// end of if
-        if(drive % 2 == 0) {
-          if ( x != 0 && y != 0) {
-            // Rotate right
-            LFPower = y - x;
-            RFPower = y + x;
-            LRPower = y - x;
-            RRPower = y + x;
-          }//end of if
-        }else if(drive % 2 == 1){
-          if( x < 0) {
-            //move left
-            LFPower = -x;
-            RFPower = x;
-            LRPower = x;
-            RRPower = -x;
-          } else if ( x > 0) {
-            //move right
-            LFPower = x;
-            RFPower = -x;
-            LRPower = -x;
-            RRPower = x;
-          } else if (y != 0){
-            //forward
-            LFPower = y;
-            RFPower = y;
-            LRPower = y;
-            RRPower = y;
-          }//end of if
-        }// end of if
+        if ( x2 != 0) {
+          // turn
+          LFPower = y - x1;
+          RFPower = y + x1;
+          LRPower = y - x1;
+          RRPower = y + x1;
+        }//end of if
+        if (x1 > 0){
+          // move right
+          LFPower = x1;
+          RFPower = -x1;
+          LRPower = -x1;
+          RRPower = x1;
+        }else if (x1 < 0){
+          LFPower = -x1;
+          RFPower = x1;
+          LRPower = x1;
+          RRPower = -x1;
+        }
+
 
 
         LFDrive.setPower(LFPower);
         RFDrive.setPower(RFPower);
         LRDrive.setPower(LRPower);
         RRDrive.setPower(RRPower);
+        SlideMotor.setPower(slidePower);
         //insert arm and servo code here
 
 
