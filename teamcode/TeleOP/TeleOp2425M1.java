6776c05287fb21319192a1b972107b08a24780e9
PastelNinjaK <bernalraven11@gmail.com>
2025-01-07 19:05:52 -0700

    Update TeleOp2425M1.java

--- teamcode/TeleOP/TeleOp2425M1.java
+++ teamcode/TeleOP/TeleOp2425M1.java
@@ -15,9 +15,13 @@ public class TeleOp2425M1 extends LinearOpMode {
   private DcMotor RFDrive;
   private DcMotor LRDrive;
   private DcMotor RRDrive;
-  private DcMotor SlideMotor;
-  private Servo intake; // Declare the servo
-  private Servo wrist;
+  double LFPower;
+  double RFPower;
+  double LRPower;
+  double RRPower;
+  // private DcMotor SlideMotor;
+  // private Servo TiltServo; // Declare the servo
+  // private Servo IntakeServo;
 
   @Override
   public void runOpMode() {
@@ -26,17 +30,20 @@ public class TeleOp2425M1 extends LinearOpMode {
     RFDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
     LRDrive = hardwareMap.get(DcMotor.class, "left_rear_drive");
     RRDrive = hardwareMap.get(DcMotor.class, "right_rear_drive");
-    SlideMotor = hardwareMap.get(DcMotor.class, "slide_motor");
-    wrist = hardwareMap.get(Servo.class, "wrist_servo");
-    intake = hardwareMap.get(Servo.class, "intake_servo");
-    wrist.setPosition(0);// to be adjusted
+    // SlideMotor = hardwareMap.get(DcMotor.class, "slide_motor");
+    // TiltServo = hardwareMap.get(Servo.class, "tilt_servo");
+    // IntakeServo = hardwareMap.get(Servo.class, "intake_servo");
+    // TiltServo.setPosition(0);// to be adjusted
+    // IntakeServo.setPosition(0);// to be adjusted(OPEN)
     //Brake mode
     LFDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     LRDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     RFDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     RRDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-    SlideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    // SlideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     // Reverse necessary motor direction
+    LRDrive.setDirection(DcMotor.Direction.REVERSE);
+    RFDrive.setDirection(DcMotor.Direction.REVERSE);
 
 
 ;
@@ -47,37 +54,38 @@ public class TeleOp2425M1 extends LinearOpMode {
         // Drive control
         double y = gamepad1.left_stick_y;
         double x1 = gamepad1.left_stick_x;
-        double x2 = gamepad.right_stick_x;
-        double LFPower;
-        double RFPower;
-        double LRPower;
-        double RRPower;
-        double slidePower = gamepad1.right_trigger - gamepad1.left_trigger;
-        int wristServo = 0;
-        if(gamepad1.a){
-          wristServo++;
-        }// end of if
-        if(y != 0) {
-          if (x2 != 0) {
-            // turn
-            LFPower = y - x1;
-            RFPower = y + x1;
-            LRPower = y - x1;
-            RRPower = y + x1;
-          }//end of if
-          if (x1 > 0) {
+        double x2 = gamepad1.right_stick_x;
+
+        LFPower = y - x2;
+        RFPower = y + x2;
+        LRPower = y - x2;
+        RRPower = y + x2;
+        // double slidePower = gamepad1.right_trigger - gamepad1.left_trigger;
+        // int tilt = 0;
+        // int intake = 0;
+
+        // if(y != 0){
+        //   if (x2 != 0) {
+        //     // turn
+        //     LFPower = y - x2;
+        //     RFPower = y + x2;
+        //     LRPower = y - x2;
+        //     RRPower = y + x2;
+        //   }//end of if
+          if (gamepad1.x && x1 > 0) {
             // move right
             LFPower = x1;
             RFPower = -x1;
             LRPower = -x1;
             RRPower = x1;
-          } else if (x1 < 0) {
+          } 
+          if (x1 < 0) {
             LFPower = -x1;
             RFPower = x1;
             LRPower = x1;
             RRPower = -x1;
           }//end of if
-        }
+        // }
 
 
 
@@ -85,10 +93,27 @@ public class TeleOp2425M1 extends LinearOpMode {
         RFDrive.setPower(RFPower);
         LRDrive.setPower(LRPower);
         RRDrive.setPower(RRPower);
-        SlideMotor.setPower(slidePower);
-        //insert arm and servo code here
+        // SlideMotor.setPower(slidePower);
+        // //insert arm and servo code here
 
-
+        // if(gamepad1.a){
+        //   tilt++;
+        // }// end of if
+        // if(tilt % 2 == 0){
+        //   //servo is straight
+        //   TiltServo.setPosition(0);
+        // }else if(tiltServo == 1){
+        //   //Servo is tilted
+        //   TiltServo.setPosition(0.45);
+        // }//end of if
+        // if(gamepad1.x){
+        //   TiltServo.setPosition(0.6);
+        //   IntakeServo.setPosition(0);//Open
+        //   TimeUnit.SECONDS.sleep(1);
+        //   IntakeServo.setPosition(0.45);//Close
+        //   TimeUnit.SECONDS.sleep(1);
+        //   TiltServo.setPosition(0.45);
+        // }//end of if
 
       }
     }
