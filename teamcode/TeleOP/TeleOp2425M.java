d59d34d1eb1ac469048bc2549aa44cb265b2fd0e
PastelNinjaK <bernalraven11@gmail.com>
2025-01-26 16:16:07 -0700

    Update TeleOp2425M.java

--- teamcode/TeleOP/TeleOp2425M.java
+++ teamcode/TeleOP/TeleOp2425M.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -9,15 +8,18 @@ import com.qualcomm.robotcore.hardware.CRServo;
 
 
 
-@TeleOp(name = "TeleOp2425M")
-public class TeleOp2425M extends LinearOpMode {
+@TeleOp(name = "TeleOp2425M1")
+public class TeleOp2425M1 extends LinearOpMode {
   private DcMotor LFDrive;
   private DcMotor RFDrive;
   private DcMotor LRDrive;
   private DcMotor RRDrive;
   private DcMotor SlideMotor;
-  private CRServo intake; // Declare the servo
-  private Servo wrist;
+  private Servo TiltServoR;
+  private Servo TiltServoL;
+  double SlidePosition = 0
+
+  // private Servo IntakeServo;
 
   @Override
   public void runOpMode() {
@@ -27,116 +29,91 @@ public class TeleOp2425M extends LinearOpMode {
     LRDrive = hardwareMap.get(DcMotor.class, "left_rear_drive");
     RRDrive = hardwareMap.get(DcMotor.class, "right_rear_drive");
     SlideMotor = hardwareMap.get(DcMotor.class, "slide_motor");
-    wrist = hardwareMap.get(Servo.class, "wrist_servo");
-    intake = hardwareMap.get(Servo.class, "intake_servo");
-    wrist.setPosition(0);// to be adjusted
-
-    // Reverse necessary motor directions
+    TiltServoR = hardwareMap.get(Servo.class, "right_tilt_servo");
+    TiltServoL = hardwareMap.get(Servo.class, "left_tilt_servo");
+    final double arm_ticks_per_degree = 0.404;
+    // IntakeServo = hardwareMap.get(Servo.class, "intake_servo");
+    TiltServoR.setPosition(0);
+    TiltServoL.setPosition(0.025);
+    // IntakeServo.setPosition(0);// to be adjusted(OPEN)
+    //Brake mode
+    LFDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    LRDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    RFDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    RRDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    SlideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    // Reverse necessary motor direction
+    LRDrive.setDirection(DcMotor.Direction.REVERSE);
     RFDrive.setDirection(DcMotor.Direction.REVERSE);
-    RRDrive.setDirection(DcMotor.Direction.REVERSE);
-    slideMotor.setDirection(DcMotor.Direction.REVERSE);
+    TiltServoR.setDirection(Servo.Direction.REVERSE);
 
 
-;
+
+    ;
     waitForStart();
 
     if (opModeIsActive()) {
       while (opModeIsActive()) {
         // Drive control
         double y = gamepad1.left_stick_y;
-        double x = gamepad1.left_stick_x;
-        double LFPower;
-        double RFPower;
-        double LRPower;
-        double RRPower;
-        double diagonal = phythagoras(x,y);
-        int drive = 0;
-
-
-// Movements
-        if(gamepad1.a){
-          drive++;
+        double x1 = gamepad1.left_stick_x;
+        double x2 = gamepad1.right_stick_x;
+        //option 1
+        double LFPower = y - x2 - x1;
+        double RFPower = y + x2 + x1;
+        double LRPower = y - x2 + x1;
+        double RRPower = y + x2 - x1;
+        //option 2
+        // double LFPower = y - x1 - x2;
+        // double RFPower = y + x1 + x2;
+        // double LRPower = y - x1 + x2;
+        // double RRPower = y + x1 - x2;
+        // double slidePower = gamepad1.left_trigger - gamepad1.right_trigger;
+        int tilt = 0;
+        // int intake = 0;
+        if(gamepad1.left_trigger != 0){
+          double left_power_factor = gamepad1.left_trigger;
+          SlidePosition -= 1 * left_power_factor;
         }
-        if(y != 0 && x == 0){
-          // Forward and backward
-          LFPower = y;
-          RFPower = y;
-          LRPower = y;
-          RRPower = y;
-        }else if(y > 0 && x > 0 && drive == 1){
-          // Quadrant 1 movement
-          LFPower = diagonal;
-          RFPower = 0;
-          LRPower = 0;
-          RRPower = diagonal;
-        }else if(y > 0 && x < 0 && drive == 1){
-          // Quadrant 2 movement
-          LFPower = 0;
-          RFPower = diagonal;
-          LRPower = diagonal;
-          RRPower = 0;
-        }else if(y < 0 && x < 0 && drive == 1){
-          // Quadrant 3 movement
-          LFPower = 0;
-          RFPower = diagonal * -1;
-          LRPower = diagonal * -1;
-          RRPower = 0;
-        }else if(y < 0 && x > 0 && drive == 1){
-          // Quadrant 4 movement
-          LFPower = diagonal * -1;
-          RFPower = 0;
-          LRPower = 0;
-          RRPower = diagonal * -1;
-        }else if(drive == 2){
-          // Rotate left
-          LFPower = x * -1;
-          RFPower = x;
-          LRPower = x * -1;
-          RRPower = x;
-        }else if(drive == 2){
-          // Rotate right
-          LFPower = x;
-          RFPower = x * -1;
-          LRPower = x;
-          RRPower = x * -1;
-        }// end of if
-
+        if(gamepad.right_trigger != 0){
+          double right_power_factor = gamepad1.right_trigger;
+          SlidePosition += 1 * right_power_factor;
+        }
         LFDrive.setPower(LFPower);
         RFDrive.setPower(RFPower);
         LRDrive.setPower(LRPower);
         RRDrive.setPower(RRPower);
-// Servo control
+        SlideMotor.setPower(slidePower);
+        // //insert arm and servo code here
 
-//        double servoPosition = wheelServo.getPower();
-//
-//        if (gamepad1.dpad_left) {
-//          servoPosition -= 100;
-//        } else if (gamepad1.dpad_right) {
-//          servoPosition += 100;
-//        } else if (gamepad1.dpad_up) {
-//          servoPosition += 50;
-//        } else if (gamepad1.dpad_down) {
-//          servoPosition -= 50;
-//        } else {
-//          servoPosition = 0;
-//        }
-//
-//
-//        wheelServo.setPower(servoPosition);
+        // if(gamepad1.a){
+        //   tilt++;
+        // }// end of if
+        if(gamepad1.dpad_up){
+          //servo is straight
+          TiltServoR.setPosition(0);
+          TiltServoL.setPosition(0.025);
+
+        }//end of if
+        if(gamepad1.dpad_down){
+          //Servo is tilted
+          TiltServoR.setPosition(0.35);
+          TiltServoL.setPosition(0.375);
+
+        }//end of if
+        // if(gamepad1.x){
+        //   TiltServo.setPosition(0.6);
+        //   IntakeServo.setPosition(0);//Open
+        //   TimeUnit.SECONDS.sleep(1);
+        //   IntakeServo.setPosition(0.45);//Close
+        //   TimeUnit.SECONDS.sleep(1);
+        //   TiltServo.setPosition(0.45);
+        // }//end of if
+
       }
     }
   }
 
-  public double squared(double base){
-    return base*base;
-  }
-  public double squareroot(double num){
-    return num/num;
-  }
-  public double phythagoras(double a,double b){
-    return squareroot(squared(a)+squared(b));
-  }
 }
-
 
 
